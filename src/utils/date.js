ef0014da9bd5161a8af667e0b9e4f96c0bfe7b82
luanchequetto <luan.gaertner900@gmail.com>
2022-03-24 08:36:12 -0300

    added date-fns for easy data manipulation

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,74 +1,21 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
 export const dateConvert = (unixDate, option) => {
   let date = new Date(unixDate * 1000);
-  let weekDay = date.getDay();
-  let day = date.getUTCDate();
-  let hour = date.getHours();
-  let minutes = date.getMinutes();
-  let month = date.getMonth();
-  const formatedMonth = [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez",
-  ];
 
   if (option === "fulldate") {
-    return `${hour > 9 ? hour : "0" + hour}:${
-      minutes > 9 ? minutes : "0" + minutes
-    } ${formatedMonth[month]} ${day}`;
+    return format(date, 'HH:mm MMM', { locale: ptBR })
+
   }
   if (option === "cardsInfo") {
-    return `${hour > 9 ? hour : "0" + hour}:${
-      minutes > 9 ? minutes : "0" + minutes
-    }`;
+    return format(date, 'HH:mm', { locale: ptBR })
   }
   if (option === "weekdayfull") {
-    switch (weekDay) {
-      case 0:
-        return "Domingo";
-      case 1:
-        return "Segunda-Feira";
-      case 2:
-        return "Terça-Feira";
-      case 3:
-        return "Quarta-Feira";
-      case 4:
-        return "Quinta-Feira";
-      case 5:
-        return "Sexta-Feira";
-      case 6:
-        return "Sábado";
-
-      default:
-        return "Hoje";
-    }
+    const weekDay = format(date, 'EEEE', { locale: ptBR })
+    let capitalized = weekDay[0].toUpperCase() + weekDay.substring(1)
+    return capitalized
   }
 
-  switch (weekDay) {
-    case 0:
-      return "DOM";
-    case 1:
-      return "SEG";
-    case 2:
-      return "TER";
-    case 3:
-      return "QUA";
-    case 4:
-      return "QUI";
-    case 5:
-      return "SEX";
-    case 6:
-      return "SAB";
-
-    default:
-      break;
-  }
+  return format(date, 'EEEEEE', { locale: ptBR }).toUpperCase()
 };
